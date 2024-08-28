29e5a9ce772ff9fd480482e489e2d96a2a3bf93c
Emma Storberg <emma.storberg@gmail.com>
2024-08-28 18:36:26 +0200

    tried to plot amplification factors but dont quite understand it.

--- projects/exponential_growth.py
+++ projects/exponential_growth.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+#from scitools.std import *
+import sys
 
 def solver(I, a, T, dt, theta):
     """Solve u'=-a*u, u(0)=I, for t in (0, T] with steps of dt."""
@@ -12,40 +14,79 @@ def solver(I, a, T, dt, theta):
         u[n+1] = (1 - (1-theta)*a*dt)/(1 + theta*dt*a)*u[n]
     return u, t
 
-dt_list = [(0.8, "#4E79A7"), (0.5, "#F28E2B"), (0.2, "#76B7B2"), (0.1,"#E15759") , (0.01,"#B07AA1")]
+def exact(t, I=1, a=-1):
+    return I*np.exp(-a*t)
 
-fig, axs = plt.subplots(1, 3, figsize=(16, 9))
+def A(t, theta, a=-1):
+    return (1 - (1-theta)*a*t)/(1 + theta*t*a)
 
-for dt in dt_list:
-    FE, t = solver(I=1, a=-1, T=8, dt=dt[0], theta=0)    # Forward Euler schemes
-    axs[0].plot(t, FE, label=f"dt = {dt[0]}", color=dt[1])
-    axs[0].set_title("Forward Euler schemes (theta = 0)")
+def amplification_factor(names, t):
+    curves = {}
+    curves['exact'] = exact(t)
+    plt.plot(t, curves['exact'])
+    #plt.hold('on')
+    name2theta = dict(FE=0, BE=1, CN=0.5)
+    for name in names:
+        curves[name] = A(t, name2theta[name])
+        plt.plot(t, curves[name])
+    plt.plot([t[0], t[-1]], [0, 0], '--')  # A=0 line
+    plt.title('Amplification factors')
+    plt.grid('on')
+    plt.legend(['exact'] + names, loc='lower left', fancybox=True)
+    plt.xlabel(r'$t=a\Delta t$')
+    plt.ylabel('Amplification factor')
+    plt.show()
+
+if __name__ == '__main__':
+
+    dt_list = [(2.1, "#ffc100"), (1.25, "#c356ea"), (0.9, "#8ff243"), (0.2,"#71aef2") , (0.1,"#ea5645")]
+
+    fig, axs = plt.subplots(1, 3, figsize=(16, 9))
+
+    for dt in dt_list:
+        FE, t = solver(I=1, a=-1, T=8, dt=dt[0], theta=0)    # Forward Euler schemes
+        axs[0].plot(t, FE, label=rf"$\Delta$t = {dt[0]}", color=dt[1])
+
+    axs[0].plot(t, exact(t), label="Exact solution", color="black",linestyle="--")
+    axs[0].set_title(r"Forward Euler schemes ($\theta = 0$)")
     axs[0].set_xlabel('t')
     axs[0].set_ylabel('Solution')
-    axs[0].set_ylim(0, 5000)
+    axs[0].set_ylim(0, 4000)
     axs[0].legend()
     axs[0].grid(True)
 
-for dt in dt_list:
-    CN, t = solver(I=1, a=-1, T=8, dt=dt[0], theta=0.5)  # Crank-Nicolson schemes
-    axs[1].plot(t, CN, label=f"dt = {dt[0]}", color=dt[1])
-    axs[1].set_title('Crank-Nicolson schemes (theta = 0.5)')
+
+    for dt in dt_list:
+        CN, t = solver(I=1, a=-1, T=8, dt=dt[0], theta=0.5)  # Crank-Nicolson schemes
+        axs[1].plot(t, CN, label=rf"$\Delta$t = {dt[0]}", color=dt[1])
+
+    axs[1].plot(t, exact(t), label="Exact solution", color="black",linestyle="--")
+    axs[1].set_title(r'Crank-Nicolson schemes ($\theta = 0.5$)')
     axs[1].set_xlabel('t')
     axs[1].set_ylabel('Solution')
-    axs[1].set_ylim(0, 5000)
+    axs[1].set_ylim(0, 4000)
     axs[1].legend()
     axs[1].grid(True)
 
-for dt in dt_list:
-    BE, t = solver(I=1, a=-1, T=8, dt=dt[0], theta=1)    # Backward Euler schemes
-    axs[2].plot(t, BE, label=f"dt = {dt[0]}", color=dt[1])
-    axs[2].set_title('Backward Euler schemes (theta = 1)')
+    for dt in dt_list:
+        BE, t = solver(I=1, a=-1, T=8, dt=dt[0], theta=1)    # Backward Euler schemes
+        axs[2].plot(t, BE, label=rf"$\Delta$t = {dt[0]}", color=dt[1])
+
+    axs[2].plot(t, exact(t), label="Exact solution", color="black",linestyle="--")
+    axs[2].set_title(r'Backward Euler schemes ($\theta = 1$)')
     axs[2].set_xlabel('t')
     axs[2].set_ylabel('Solution')
-    axs[2].set_ylim(0, 5000)
+    axs[2].set_ylim(0, 4000)
     axs[2].legend()
     axs[2].grid(True)
 
-# Adjust layout and show the figure
-plt.tight_layout()
-plt.show()
+    # Adjust layout and show the figure
+    plt.tight_layout()
+    plt.show()
+
+
+    if len(sys.argv) > 1:
+        names = sys.argv[1:]
+    else: # default
+        names = 'FE BE CN'.split()
+    amplification_factor(names, t)
